e6c2849e1bea8b7039e2a3f822f4e3f92e0f325f
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-04 21:25:57 +0800

    added custom marker to maps in explore screen

---
+++ components/android/explore/CustomMarker.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Marker, Callout } from 'react-native-maps';
+import { Text, useTheme } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+
+export default function CustomMarker() {
+  const { fonts, colors } = useTheme();
+  const styles = StyleSheet.create({
+    tooltip: {
+      width: 220,
+      borderColor: '#e0e0e0',
+      borderWidth: 1,
+      borderRadius: 8,
+      marginBottom: 4,
+      backgroundColor: 'white',
+      overflow: 'hidden',
+    },
+    image: {
+      width: '100%',
+      height: 140,
+      backgroundColor: '#e0e0e0',
+      resizeMode: 'cover',
+    },
+    detail: {
+      paddingVertical: 12,
+      paddingHorizontal: 16,
+    },
+    detail_title: {
+      ...fonts.medium,
+      marginBottom: 4,
+    },
+    detail_author: {
+      marginBottom: 8,
+      color: colors.placeholder,
+    },
+    detail_time: {
+      fontSize: 12,
+      color: colors.placeholder,
+    },
+  });
+  return (
+    <Marker
+      title="Ashiap"
+      description="This is ashiap mantuls"
+      pinColor="aqua"
+      coordinate={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+      }}
+    >
+      <Callout tooltip>
+        <View style={styles.tooltip}>
+          <View style={styles.image} />
+          <View style={styles.detail}>
+            <Text style={styles.detail_title}>
+              Usai Mencuri, Dua Pemuda Ditangkap Pesta Narkoba
+            </Text>
+            <Text style={styles.detail_author}>Pandean Mertayasa</Text>
+            <Text style={styles.detail_time}>Selasa, 23 April 2020</Text>
+          </View>
+        </View>
+      </Callout>
+    </Marker>
+  );
+}
--- screens/MainDrawer/ExploreScreen.jsx
+++ screens/MainDrawer/ExploreScreen.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, useWindowDimensions } from 'react-native';
+
+import CustomMarker from '../../components/android/explore/CustomMarker';
 
 export default function HomeScreen() {
+  const { width, height } = useWindowDimensions();
+  const styles = StyleSheet.create({
+    map: {
+      width,
+      height,
+    },
+  });
   return (
-    <View>
-      <Text>Explore screen</Text>
-    </View>
+    <>
+      <MapView
+        showTraffics
+        loadingEnabled
+        rotateEnabled={false}
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+        <CustomMarker />
+      </MapView>
+    </>
   );
 }
