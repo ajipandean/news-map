242e6d49d476f734ca934eb2df77fc6671c361fe
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-04 18:26:52 +0800

    added header right icon for explore screen

--- registers/mainDrawerRegister.jsx
+++ registers/mainDrawerRegister.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import CreateEventIcon from '../components/android/CreateEventIcon';
+
 import ExploreScreen from '../screens/MainDrawer/ExploreScreen';
 
 export default [
@@ -12,6 +16,7 @@ export default [
         borderColor: '#e0e0e0',
         borderBottomWidth: 1,
       },
+      headerRight: () => <CreateEventIcon />,
     },
   },
 ];
