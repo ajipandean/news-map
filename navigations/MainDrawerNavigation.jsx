7c6a6be3864ee957bd96673f0b74f02bdee3914e
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-04 18:11:05 +0800

    configured main drawer navigator and drawer

--- navigations/MainDrawerNavigation.jsx
+++ navigations/MainDrawerNavigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import mainDrawerRegister from '../registers/mainDrawerRegister';
@@ -7,13 +8,35 @@ const MainDrawer = createDrawerNavigator();
 
 export default function MainDrawerNavigation() {
   return (
-    <MainDrawer.Navigator>
+    <MainDrawer.Navigator
+      initialRouteName="home"
+      screenOptions={{ headerShown: true }}
+      drawerType="slide"
+      drawerStyle={{
+        borderColor: '#e0e0e0',
+        borderRightWidth: 1,
+      }}
+      overlayColor={1}
+      drawerContentOptions={{
+        labelStyle: { fontWeight: 'bold' },
+        itemStyle: { marginVertical: 0 },
+      }}
+    >
       {mainDrawerRegister.map((s) => (
         <MainDrawer.Screen
           key={s.name}
           name={s.name}
           component={s.component}
-          options={s.options}
+          options={{
+            ...s.options,
+            drawerIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name={s.icon}
+                color={color}
+                size={size}
+              />
+            ),
+          }}
         />
       ))}
     </MainDrawer.Navigator>
--- registers/mainDrawerRegister.jsx
+++ registers/mainDrawerRegister.jsx
@@ -3,7 +3,10 @@ import ExploreScreen from '../screens/MainDrawer/ExploreScreen';
 export default [
   {
     name: 'explore',
+    icon: 'compass-outline',
     component: ExploreScreen,
-    options: {},
+    options: {
+      title: 'Explore',
+    },
   },
 ];
