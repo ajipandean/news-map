1c20a1ed26a038f1494954531b70981e5807cf72
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-06 11:07:47 +0800

    added camera screen

--- registers/RootStackRegister.jsx
+++ registers/RootStackRegister.jsx
@@ -1,9 +1,17 @@
 import MainBottomTabsNavigation from '../navigations/MainBottomTabsNavigation';
+import CameraScreen from '../screens/RootStack/CameraScreen';
 
 export default [
   {
     name: 'main-bottom-tabs',
     component: MainBottomTabsNavigation,
+    options: {
+      headerShown: false,
+    },
+  },
+  {
+    name: 'camera',
+    component: CameraScreen,
     options: {
       headerShown: false,
     },
