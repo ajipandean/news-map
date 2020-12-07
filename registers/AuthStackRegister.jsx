a98bb9afd91a11cb4689fa8cc1acef559afa8678
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-07 14:13:23 +0800

    added register screen

--- registers/AuthStackRegister.jsx
+++ registers/AuthStackRegister.jsx
@@ -1,9 +1,17 @@
 import LoginScreen from '../screens/AuthStack/LoginScreen';
+import RegisterScreen from '../screens/AuthStack/RegisterScreen';
 
 export default [
   {
     name: 'login',
     component: LoginScreen,
+    options: {
+      headerShown: false,
+    },
+  },
+  {
+    name: 'register',
+    component: RegisterScreen,
     options: {
       headerShown: false,
     },
---
+++ screens/AuthStack/RegisterScreen.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import {
+  View, StyleSheet, TouchableOpacity, Text,
+} from 'react-native';
+import {
+  TextInput, Title, Button, useTheme,
+} from 'react-native-paper';
+
+export default function LoginScreen() {
+  const { colors } = useTheme();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: colors.surface,
+    },
+    heading: { textAlign: 'center' },
+    form: { padding: 16 },
+    input: { marginBottom: 16 },
+    button: {
+      marginTop: 24,
+    },
+    helper: { alignItems: 'center' },
+    helper_text: {
+      fontSize: 12,
+      color: colors.placeholder,
+    },
+  });
+  return (
+    <View style={styles.container}>
+      <Title style={styles.heading}>Register</Title>
+      <View style={styles.form}>
+        <TextInput
+          dense
+          autoFocus
+          mode="outlined"
+          placeholder="Email"
+          style={styles.input}
+          value={email}
+          onChangeText={(v) => setEmail(v)}
+        />
+        <TextInput
+          dense
+          secureTextEntry
+          mode="outlined"
+          placeholder="Password"
+          style={styles.input}
+          value={password}
+          onChangeText={(v) => setPassword(v)}
+        />
+        <TextInput
+          dense
+          secureTextEntry
+          mode="outlined"
+          placeholder="Password"
+          style={styles.input}
+          value={confirm}
+          onChangeText={(v) => setConfirm(v)}
+        />
+        <Button
+          loading={false}
+          mode="contained"
+          onPress={() => console.log(email, password)}
+        >
+          Register
+        </Button>
+      </View>
+      <TouchableOpacity style={styles.helper}>
+        <Text style={styles.helper_text}>Don&apos;t have an account? Register here.</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
