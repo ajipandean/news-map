5a5d79635b4bd6cb048478da2816d1e6a1e8273d
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-08 06:52:29 +0800

    searched token in async storage, if exists redirect

--- navigations/RootStackNavigation.jsx
+++ navigations/RootStackNavigation.jsx
@@ -1,16 +1,57 @@
-import React, { useState } from 'react';
+import React, { useReducer, useEffect } from 'react';
+import { ToastAndroid } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import RootStackRegister from '../registers/RootStackRegister';
 import AuthStackRegister from '../registers/AuthStackRegister';
+import firebase from '../firebase.config';
 
 const RootStack = createStackNavigator();
 
+const initialState = {
+  isLogout: false,
+  token: null,
+};
+
 export default function RootStackNavigation() {
-  const [login, setLogin] = useState(false);
+  const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+        case 'LOGIN':
+          return {
+            ...prevState,
+            isLogout: false,
+            token: action.token,
+          };
+        case 'LOGOUT':
+          return {
+            ...prevState,
+            isLogout: true,
+            token: null,
+          };
+        default:
+          return prevState;
+      }
+    },
+    initialState,
+  );
+  useEffect(() => {
+    (async () => {
+      let token;
+      try {
+        token = await AsyncStorage.getItem('token');
+      } catch (err) {
+        token = null;
+        ToastAndroid.show(err.message, ToastAndroid.LONG);
+      }
+      dispatch({ type: 'RESTORE_TOKEN', token });
+    })();
+  }, []);
   return (
     <RootStack.Navigator>
-      {login ? (
+      {state.token ? (
         <>
           {RootStackRegister.map((s) => (
             <RootStack.Screen
