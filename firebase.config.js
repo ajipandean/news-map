44446e3b4f826514e24c3db7900fc73e2308d2e2
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-08 06:41:41 +0800

    added firebase config

---
+++ firebase.config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyC0yGREXdI3qpaRshYf_LSirKrMSp1gs10',
+  authDomain: 'news-app-6d814.firebaseapp.com',
+  projectId: 'news-app-6d814',
+  storageBucket: 'news-app-6d814.appspot.com',
+  messagingSenderId: '538120566824',
+  appId: '1:538120566824:web:6de3de25c5ae251cbf5d1d',
+  measurementId: 'G-4CH8SF3GG8',
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(config);
+}
+
+export default firebase;
