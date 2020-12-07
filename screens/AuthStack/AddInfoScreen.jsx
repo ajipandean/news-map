c903093cebea55170c398cb6fc0518ee1bad17dd
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-08 05:38:50 +0800

    added ui in add-info screen

--- screens/AuthStack/AddInfoScreen.jsx
+++ screens/AuthStack/AddInfoScreen.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, StyleSheet, Image, TouchableOpacity,
+} from 'react-native';
+import { TextInput, Button, useTheme } from 'react-native-paper';
 
 export default function AddInfoScreen() {
+  const { colors } = useTheme();
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: colors.surface,
+    },
+    image_wrapper: {
+      alignSelf: 'center',
+    },
+    avatar: {
+      width: 120,
+      height: 120,
+      resizeMode: 'cover',
+      borderRadius: 100,
+    },
+    form: { padding: 16 },
+    input: { marginBottom: 16 },
+  });
   return (
-    <View>
-      <Text>Add info screen</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.image_wrapper}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://style.anu.edu.au/_anu/4/images/placeholders/person.png' }}
+        />
+      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput
+          dense
+          mode="outlined"
+          style={styles.input}
+          placeholder="Enter display name"
+        />
+        <Button
+          mode="contained"
+        >
+          Save
+        </Button>
+      </View>
     </View>
   );
 }
