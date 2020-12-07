c75e555f21fa2690d46bed22e890989bcf06b888
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-07 13:51:46 +0800

    added auth stack

--- navigations/RootStackNavigation.jsx
+++ navigations/RootStackNavigation.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import RootStackRegister from '../registers/RootStackRegister';
+import AuthStackRegister from '../registers/AuthStackRegister';
 
 const RootStack = createStackNavigator();
 
 export default function RootStackNavigation() {
+  const [login, setLogin] = useState(false);
   return (
     <RootStack.Navigator>
-      {RootStackRegister.map((s) => (
-        <RootStack.Screen
-          key={s.name}
-          name={s.name}
-          component={s.component}
-          options={{
-            ...s.options,
-          }}
-        />
-      ))}
+      {login ? (
+        <>
+          {RootStackRegister.map((s) => (
+            <RootStack.Screen
+              key={s.name}
+              name={s.name}
+              component={s.component}
+              options={{
+                ...s.options,
+              }}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          {AuthStackRegister.map((s) => (
+            <RootStack.Screen
+              key={s.name}
+              name={s.name}
+              component={s.component}
+              options={{
+                ...s.options,
+              }}
+            />
+          ))}
+        </>
+      )}
     </RootStack.Navigator>
   );
 }
---
+++ registers/AuthStackRegister.jsx
@@ -0,0 +1,11 @@
+import LoginScreen from '../screens/AuthStack/LoginScreen';
+
+export default [
+  {
+    name: 'login',
+    component: LoginScreen,
+    options: {
+      headerShown: false,
+    },
+  },
+];
