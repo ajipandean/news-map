ff838abcb25503e07cf437cf7fc80521b358c926
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-08 05:28:03 +0800

    added add-info screen

--- registers/AuthStackRegister.jsx
+++ registers/AuthStackRegister.jsx
@@ -1,5 +1,6 @@
 import LoginScreen from '../screens/AuthStack/LoginScreen';
 import RegisterScreen from '../screens/AuthStack/RegisterScreen';
+import AddInfoScreen from '../screens/AuthStack/AddInfoScreen';
 
 export default [
   {
@@ -15,5 +16,10 @@ export default [
     options: {
       headerShown: false,
     },
+  },
+  {
+    name: 'add-info',
+    component: AddInfoScreen,
+    options: { headerShown: false },
   },
 ];
