a332f05c77a09576e9f93143fc69b668d9647d4b
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-12-08 13:05:42 +0800

    added logout feature

--- screens/MainBottomTabs/AccountScreen.jsx
+++ screens/MainBottomTabs/AccountScreen.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import {
-  View, Image, Text, StyleSheet,
+  View, Image, StyleSheet, ToastAndroid,
 } from 'react-native';
 import {
   Appbar, useTheme, Title, Paragraph, Button,
 } from 'react-native-paper';
 
+import firebase from '../../firebase.config';
+import AuthContext from '../../context/AuthContext';
+
 export default function AccountScreen() {
   const { colors } = useTheme();
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(false);
+  const { logout } = useContext(AuthContext);
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((u) => {
+      if (user) {
+        setUser(u);
+      }
+    });
+    return () => unsubscribe();
+  });
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -32,28 +46,50 @@ export default function AccountScreen() {
       marginBottom: 16,
     },
   });
+  async function handleLogout() {
+    setLoading(true);
+    try {
+      await logout();
+    } catch (err) {
+      ToastAndroid.show(err.message, ToastAndroid.LONG);
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Appbar.Header style={styles.appbar}>
-        <Appbar.Content title="Profile" />
-      </Appbar.Header>
-      <View style={styles.content}>
-        <View style={styles.image_wrapper}>
-          <Image
-            style={styles.image}
-            source={{ uri: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3925efb9-68c4-4f09-a69f-ea85117d53e5/d6y6v9l-5056055b-410f-4579-ab3b-26a2568175f8.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMzkyNWVmYjktNjhjNC00ZjA5LWE2OWYtZWE4NTExN2Q1M2U1XC9kNnk2djlsLTUwNTYwNTViLTQxMGYtNDU3OS1hYjNiLTI2YTI1NjgxNzVmOC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.sxyRBEuplH-pkRNMvVF39wHKye4SYtKK4jnNlRRs_80' }}
-          />
+    <>
+      {user ? (
+        <View style={styles.container}>
+          <Appbar.Header style={styles.appbar}>
+            <Appbar.Content title="Profile" />
+          </Appbar.Header>
+          <View style={styles.content}>
+            <View style={styles.image_wrapper}>
+              <Image
+                style={styles.image}
+                source={{ uri: user.photoURL || 'https://style.anu.edu.au/_anu/4/images/placeholders/person.png' }}
+              />
+            </View>
+            <Title>{user.displayName}</Title>
+            <Paragraph style={styles.email}>{user.email}</Paragraph>
+            <Button
+              loading={loading}
+              mode="contained"
+              onPress={handleLogout}
+            >
+              Logout
+            </Button>
+          </View>
         </View>
-        <Title>Kousaka Kirino</Title>
-        <Paragraph style={styles.email}>kiririn@mail.com</Paragraph>
-        <Button
-          loading={false}
-          mode="contained"
-          onPress={() => {}}
+      ) : (
+        <View style={[styles.container, {
+          alignItems: 'center',
+          justifyContent: 'center',
+        }]}
         >
-          Logout
-        </Button>
-      </View>
-    </View>
+          <Title>User not logged in.</Title>
+        </View>
+      )}
+    </>
   );
 }
